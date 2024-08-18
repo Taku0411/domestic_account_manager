420886a4ee1fb1509b11c9fff1a7906f2195058e
Taku0411 <1991104taku@gmail.com>
2024-08-19 00:58:24 +0900

    modified css format

--- src/app/static/cash_deposit.js
+++ src/app/static/cash_deposit.js
@@ -1,151 +1,98 @@
 async function draw_cash_deposit(isFirst) {
-  var chartTotalTimeSeries = document.getElementById("chartTotalTimeSeries")
+  const container = document.getElementById("segment");
+  const chartTotalTimeSeries = document.getElementById("chartTotalTimeSeries")
   const response = await fetch("/cash_deposit")
   const data = await response.json()
-  console.log(data)
-  var TotalTimeSeries = new Chart(chartTotalTimeSeries,
-    {
-      type: "line",
-      data: {
-        labels: data["date"],
-      },
-      options: {
-        respons: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [{
-            type: "time",
-            time: {
-              unit: "day",
-              displayFormats: {
-                day: "YYYY/MM/DD"
-              }
-            },
-          }],
-          yAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: "円"
-            },
-            ticks: {
-              suggestedMin: 0,
-              callback: function (label, index, labels) {
-                return label.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-              }
-            },
-            stacked: true
-          }]
-        },
-        plugins: {
-          filter: {
-            propagate: true
-          }
-        }
-      }
-    }
-  );
-  for (let i = 0; i < data["ndata"]; i++) {
-    const i_data = data[String(i)];
-    if (i == 0) {
-      TotalTimeSeries.data.datasets.push(
-        {
-          fill: "origin",
-          stack: 1,
-          label: i_data["account_name"],
-          data: i_data["data"],
-          lineTransition: 0,
-          backgroundColor: color_list[i][0],
-          borderColor: color_list[i][1],
-        }
-      );
-    }
-    else {
-      TotalTimeSeries.data.datasets.push(
-        {
-          fill: "-1",
-          stack: 1,
-          label: i_data["account_name"],
-          data: i_data["data"],
-          lineTransition: 0,
-          backgroundColor: color_list[i][0],
-          borderColor: color_list[i][1],
-        }
-      );
-    }
-  }
-  TotalTimeSeries.update();
 
-  if (isFirst) {
-    const container = document.getElementById("segment");
-    for (let i = 0; i < Number(data["ndata"]); i++) {
-      const i_data = data[String(i)];
-      const tmp = document.createElement("div");
-      num = formatNumber(i_data["data"].at(-1));
-      tmp.className = "w-full lg:w-1/2 pl-0 mt-6 lg:pl-6 h-96";
-      const html = `<p class="text-xl pb-3 flex items-center h-1/4 lg:h-1/6">
+  const total = Highcharts.chart(chartTotalTimeSeries, {
+    chart: {
+      type: "area",
+      zooming: {
+        type: "x"
+      },
+    },
+    title: {text: ""},
+    xAxis: {
+      type: "datetime",
+    },
+    yAxis: {
+      title: {
+        text: "円",
+      },
+    },
+    credits: {
+      enabled: false
+    },
+    plotOptions: {
+      area: {
+        stacking: "normal",
+        linewidth: 1
+      },
+    },
+  },
+  );
+
+  // insert elements
+  for (let i = 0; i < Number(data["ndata"]); i++) {
+    const i_data = data[String(i)];
+    const tmp = document.createElement("div");
+    num = i_data["data"].at(-1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    tmp.className = "w-full lg:w-1/2 p-0 pb-3 lg:p-3";
+    const html = `
+            <p class="text-xl pb-3 items-center">
               <i class="fas fa-magnifying-glass-chart mr-3"></i> ${i_data["account_name"]}（${i_data["account_type"]}）：${num}円 
             </p>
-            <div class="p-3 bg-white h-3/4 lg:h-5/6">
-              <canvas id="chartTimeSeries_${i}"></canvas> `
-      tmp.innerHTML = html;
-      container.appendChild(tmp);
+            <div class="p-6 bg-white">
+              <div id="chartTimeSeries_${i}" class="h-64"></div>
+            </div>`
+    tmp.innerHTML = html;
+    container.appendChild(tmp);
 
-    }
   }
 
-  for (let i = 0; i < Number(data["ndata"]); i++) {
-    var chart_i = document.getElementById("chartTimeSeries_" + String(i));
-    var chart = new Chart(chart_i, {
-      type: "line",
-      data:
-      {
-        labels: data["date"],
-        datasets: [
-          {
-            fill: false,
-            label: data[String(i)]["account_type"],
-            data: data[String(i)]["data"],
-            lineTransition: 0,
-            borderColor: color_list[i][1],
-            backgroundColor: color_list[i][0],
-          }
+  // data
+  for (var i = 0; i < Number(data["ndata"]); i++) {
+    each_data = []
 
-        ]
-      },
-      options: {
-        respons: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [{
-            type: "time",
-            time: {
-              unit: "day",
-              displayFormats: {
-                day: "YYYY/MM/DD"
-              }
-            },
-          }],
-          yAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: "円"
-            },
-            ticks: {
-              callback: function (label, index, labels) {
-                return label.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-              }
-            },
-          }]
-        },
-        plugins: {
-          filter: {
-            propagate: true
-          }
+    // time
+    for (var j = 0; j < data["date"].length; j++) {
+      each_data.push([Date.parse(data["date"][j]), data[String(i)]["data"][j]]);
+    }
+
+    // add to total
+    total.addSeries({
+      name: data[String(i)]["account_name"],
+      data: each_data
+    });
+
+    // add element
+    var i_chart = document.getElementById("chartTimeSeries_" + String(i));
+    Highcharts.chart(i_chart, {
+      chart: {
+        type: "area",
+        zooming: {
+          type: "x"
         }
-      }
-    }
-    );
-
-    console.log(document.getElementById("segment"))
+      },
+      title: {text: ""},
+      xAxis: {
+        type: "datetime"
+      },
+      yAxis: {
+        title: {
+          text: "yen"
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      legend: {enabled: false},
+      series: [{
+        name: "",
+        data: each_data,
+        color: Highcharts.getOptions().colors[i],
+      },],
+    });
   }
+  console.log(each_data)
 }
